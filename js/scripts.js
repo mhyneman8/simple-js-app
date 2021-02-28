c5c2596d5e4d261fc53bf9e2a828dcac4f18ef1e
mhyneman8 <mhyneman@gmail.com>
2021-02-28 16:10:32 -0600

    Created pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'potatoes';
-document.write(favoriteFood);
-
-let test = 'maybe?';
+let pokemonList = [
+  {name: 'Squirtle', height: '0.5 m', type: ['Monster', 'Water'], hp: 44},
+  {name: 'Charmander', height: '0.6 m', type: ['Monster', 'Dragon'], hp: 39},
+  {name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Monster'], hp: 45},
+  {name: 'Pikachu', height: '', type: ['Electric', 'Fairy'], hp: 35}
+];
