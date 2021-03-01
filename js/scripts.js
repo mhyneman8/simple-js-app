a2fd2b090b53f8091a4bfe55a8b94d1205826534
mhyneman8 <mhyneman@gmail.com>
2021-03-01 11:28:34 -0600

    1.3 JS Added for loop and condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 let pokemonList = [
-  {name: 'Squirtle', height: '0.5 m', type: ['Monster', 'Water'], hp: 44},
-  {name: 'Charmander', height: '0.6 m', type: ['Monster', 'Dragon'], hp: 39},
-  {name: 'Bulbasaur', height: '0.7 m', type: ['Grass', 'Monster'], hp: 45},
-  {name: 'Pikachu', height: '', type: ['Electric', 'Fairy'], hp: 35}
+  {name: 'Squirtle', height: 0.5, type: ['Monster', 'Water'], hp: 44},
+  {name: 'Charmander', height: 0.6, type: ['Monster', 'Dragon'], hp: 39},
+  {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Monster'], hp: 45},
+  {name: 'Pikachu', height: 0.4, type: ['Electric', 'Fairy'], hp: 35}
 ];
+
+// prints list of pokemon in pokemonList
+for (let i = 0; i < pokemonList.length ; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m)");
+
+  // prints when pokemon is over 0.6 m
+  if (pokemonList[i].height > 0.6) {
+    document.write(" - Wow, that's big!");
+  }
+  // breaks line after each item
+  document.write('<br>');
+
+}
