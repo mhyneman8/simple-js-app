521d0ae8dea4f88d2854a48a7edf17681127a5f6
mhyneman8 <mhyneman@gmail.com>
2021-03-04 13:33:12 -0600

    added comments, fixed add error

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
-const pokemonRepository = (function() {
+const pokemonRepo = (function() {
 
-  let pokemonList = [
-    {name: 'Squirtle', height: 0.5, type: ['Monster', 'Water'], hp: 44},
-    {name: 'Charmander', height: 0.6, type: ['Monster', 'Dragon'], hp: 39},
+  let pokemonList =
+    [{name: 'Squirtle', height: 0.5, type: ['Water'], hp: 44},
+    {name: 'Charmander', height: 0.6, type: ['Fire'], hp: 39},
     {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Monster'], hp: 45},
     {name: 'Pikachu', height: 0.4, type: ['Electric', 'Fairy'], hp: 35}
   ];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-    document.write(pokemonList);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
   return {
-    add: add,
-    getAll: getAll
+    // add a pokemon, as long as it's an object
+    add: function(pokemon) {
+      if (typeof(pokemon) === 'object' && typeof(pokemon) !== null) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log('you need an object');
+      }
+    },
+    // retuns list of all pokemonList array
+    getAll: function() {
+      return pokemonList;
+    }
   };
 })();
 
-pokemonRepository.add({name: pichu, height: 0.4, type: "electric"});
+// adds pichu to pokemonList
+pokemonRepo.add({name: 'Pichu', height: 0.4, type: ["electric"]});
 
 // gets list of pokemon
-pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepo.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + " height: " + pokemon.height + " m" + "<br>" );
     if (pokemon.height > 0.6) {
      document.write(" - Wow, that's big! <br>");
